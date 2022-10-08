70685301d0f4e29ddf41560cf0542292eb11871f
Natalie Arkow <109115351+nataliearkow@users.noreply.github.com>
2022-10-08 18:55:52 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# MS-Module-9
+
+## hi
+
+## Table of Content
+- [Project description](#Description)
+- [Usage](#Usage)
+- [Contribution](#Contributing)
+- [Installation](#Installation)
+- [Questions](#Questions)
+- [License](#License)
+
+## Description
+hi
+
+## Usage
+hi
+
+## Installation
+hi
+        
+## Contributing
+undefined
+
+## Questions
+hi
+hi
+
+## License
+isc
+
+        
---
+++ index.js
@@ -0,0 +1,76 @@
+// Packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const MarkDown = require('./lib/readmegen')
+
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation Instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Information',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution Guidelines',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Test Instructions',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'For questions(email)',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'For questions(github)',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License',
+        choices: ['ISC'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+]
+
+function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        const mark = MarkDown.generateReadme(answers)
+        fs.writeFile('README.md', mark, function(err) {
+            if (err) {
+                console.log('Could not save file', err)
+            } else {
+                console.log('Success: new README.md file generated inside the current folder')
+            }
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+}
+    runQuery()
